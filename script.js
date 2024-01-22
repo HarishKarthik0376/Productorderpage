5555bd6e31084d376a3ffd9c4638e10bf4df03b0
HarishKarthik0376 <103718125+HarishKarthik0376@users.noreply.github.com>
2024-01-22 20:08:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+document.getElementById("first").addEventListener("click",function(){
+    document.getElementById("one").style.display = "block";
+    document.getElementById("two").style.display = "none";
+    document.getElementById("three").style.display = "none";
+    document.getElementById("one1").style.display = "block";
+    document.getElementById("two2").style.display = "none";
+    document.getElementById("three3").style.display = "none";
+    document.getElementById("one4").style.display = "block";
+    document.getElementById("two5").style.display = "none";
+    document.getElementById("three6").style.display = "none";
+    
+})
+document.getElementById("second").addEventListener("click",function(){
+    document.getElementById("one").style.display = "none";
+    document.getElementById("two").style.display = "block";
+    document.getElementById("three").style.display = "none";
+    document.getElementById("one1").style.display = "none";
+    document.getElementById("two2").style.display = "block";
+    document.getElementById("three3").style.display = "none";
+    document.getElementById("one4").style.display = "none";
+    document.getElementById("two5").style.display = "block";
+    document.getElementById("three6").style.display = "none";
+})
+document.getElementById("third").addEventListener("click",function(){
+    document.getElementById("one").style.display = "none";
+    document.getElementById("two").style.display = "none";
+    document.getElementById("three").style.display = "block";
+    document.getElementById("one1").style.display = "none";
+    document.getElementById("two2").style.display = "none";
+    document.getElementById("three3").style.display = "block";
+    document.getElementById("one4").style.display = "none";
+    document.getElementById("two5").style.display = "none";
+    document.getElementById("three6").style.display = "block";
+})
+document.getElementById("button1").addEventListener("click",function()
+{   
+    document.getElementById("black").style.display = "block";
+    document.getElementById("natural").style.display = "none";
+    document.getElementById("blue").style.display = "none";
+})
+document.getElementById("button2").addEventListener("click",function()
+{
+    document.getElementById("black").style.display = "none";
+    document.getElementById("natural").style.display = "block";
+    document.getElementById("blue").style.display = "none";
+   
+})
+document.getElementById("button3").addEventListener("click",function()
+{
+    document.getElementById("black").style.display = "none";
+    document.getElementById("natural").style.display = "none";
+    document.getElementById("blue").style.display = "block";
+ 
+})
